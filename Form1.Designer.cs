e88204c9e5def3e4067ab916140f31f4a4a05dd6
SerGenius1 <s.burnashev03@gmail.com>
2024-04-30 18:23:29 +0700

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace Lab_8._2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.input = new System.Windows.Forms.TextBox();
+            this.bt_Predict = new System.Windows.Forms.Button();
+            this.label_Answer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // input
+            // 
+            this.input.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.input.Location = new System.Drawing.Point(64, 57);
+            this.input.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.input.MaxLength = 200;
+            this.input.Name = "input";
+            this.input.Size = new System.Drawing.Size(611, 34);
+            this.input.TabIndex = 1;
+            this.input.Text = "Введите вопрос";
+            // 
+            // bt_Predict
+            // 
+            this.bt_Predict.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.bt_Predict.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.bt_Predict.Location = new System.Drawing.Point(279, 118);
+            this.bt_Predict.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.bt_Predict.Name = "bt_Predict";
+            this.bt_Predict.Size = new System.Drawing.Size(184, 44);
+            this.bt_Predict.TabIndex = 2;
+            this.bt_Predict.Text = "Предсказать";
+            this.bt_Predict.UseVisualStyleBackColor = true;
+            this.bt_Predict.Click += new System.EventHandler(this.bt_Predict_Click);
+            // 
+            // label_Answer
+            // 
+            this.label_Answer.AutoSize = true;
+            this.label_Answer.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_Answer.Location = new System.Drawing.Point(223, 283);
+            this.label_Answer.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label_Answer.Name = "label_Answer";
+            this.label_Answer.Size = new System.Drawing.Size(0, 29);
+            this.label_Answer.TabIndex = 3;
+            this.label_Answer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(779, 490);
+            this.Controls.Add(this.label_Answer);
+            this.Controls.Add(this.bt_Predict);
+            this.Controls.Add(this.input);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "The ball of predictions";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox input;
+        private System.Windows.Forms.Button bt_Predict;
+        private System.Windows.Forms.Label label_Answer;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace Lab_8._2
+{
+    public partial class Form1 : Form
+    {
+        private Random random;
+
+        private string[] positiveAnswers;
+        private string[] indecisiveAnswers;
+        private string[] neutralAnswers;
+        private string[] negativeAnswers;
+        private List<string[]> answers;
+        private double[] probabilities = new double[4] { 0.25, 0.15, 0.40, 0.20 };
+
+        private Dictionary<string, string> previousAnswers;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            positiveAnswers = new string[5]
+            {
+                "Это несомненно",
+                "Это определенно так",
+                "Без сомнения",
+                "Да, определенно",
+                "Вы можете положиться на это"
+            };
+
+            indecisiveAnswers = new string[5]
+            {
+                "Насколько я понимаю, да",
+                "Вероятнее всего",
+                "Перспективы хорошие",
+                "Знаки указывают на то, что да",
+                "Скорее всего да"
+            };
+
+            neutralAnswers = new string[5]
+            {
+                "Ответ туманный, попробуйте еще раз",
+                "Спросите еще раз позже",
+                "Лучше не говорить тебе сейчас",
+                "Сейчас я не могу ничего предсказать",
+                "Сосредоточься и спроси еще раз"
+            };
+
+            negativeAnswers = new string[5]
+            {
+                "Не рассчитывай на это",
+                "Мой ответ - нет",
+                "Мои источники говорят, что нет",
+                "Перспективы не очень хорошие",
+                "Очень сомнительно"
+            };
+
+            answers = new List<string[]> { positiveAnswers, indecisiveAnswers, neutralAnswers, negativeAnswers };
+
+            previousAnswers = new Dictionary<string, string>();
+
+            random = new Random();
+        }
+
+        private void bt_Predict_Click(object sender, EventArgs e)
+        {
+            var question = input.Text;
+            //Приведение всех букв к нижнему регистру и удаление всей пунктуации, чтобы она не прияла на содержание вопроса
+            question = question.ToLower();
+            question = Regex.Replace(question, "\\p{P}", string.Empty);
+            question = Regex.Replace(question, " ", string.Empty);
+
+            if (previousAnswers.ContainsKey(question))
+            {
+                label_Answer.Text = previousAnswers[question];
+                return;
+            }
+
+            double probability =  random.NextDouble();
+
+            int typeOfAnswer = 0;
+            while (probability >= 0)
+            {
+                probability -= probabilities[typeOfAnswer];
+                typeOfAnswer++;
+            }
+            typeOfAnswer--;
+
+            var answer = answers[typeOfAnswer][random.Next(0,4)];
+
+            //Если выпала секция "спросите позже", то ответ не сохраняется
+            if (typeOfAnswer != 2)
+            {
+                previousAnswers[question] = answer;
+            }
+
+            label_Answer.Text = answer;
+        }
+    }
+}
